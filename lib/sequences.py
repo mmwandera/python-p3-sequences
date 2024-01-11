508760d9195e53b1953e15b33b4befd0212fc2a2
mmwandera <mmwandera74@gmail.com>
2024-01-11 13:57:04 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    #initialise new list
+	sequence = []
+	
+    #cater for length between 0 and 1 only first.
+	if length > 0:
+		sequence.append(0)
+		
+    #cater for length equal to 2 going up second.
+	#firstly, if length = 2
+	if length >= 2:
+		sequence.append(1)
+		
+        #secondly, for length greater than 2
+		for i in range(2, length):
+			sequence.append(sequence[-1] + sequence[-2])
+
+	print(sequence)
